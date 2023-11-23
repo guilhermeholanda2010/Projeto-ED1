38610ed26c8c8ac3a051c3a97c61107a95bf0dc7
Guilherme Holanda <guilhermeholanda519@gmail.com>
2023-11-23 01:00:51 -0300

    commit inicial

---
+++ src/filmes/Categoria.java
@@ -0,0 +1,5 @@
+package filmes;
+
+public class Categoria {
+  
+}
---
+++ src/filmes/aplicacao.java
@@ -0,0 +1,64 @@
+package filmes;
+import java.util.*;
+
+public class aplicacao {
+  public static void main(String[] args) {
+
+public class aplicacao {
+    public static void main(String[] args){
+        ListaFilmes listaFilmes = new ListaFilmes();
+        Filme filme = new Filme();
+        Scanner in = new Scanner(System.in);
+        int opcao;
+
+        do {
+            System.out.println("Digite 1 para inserir um filme:");
+            System.out.println("Digite 2 para remover um filme:");
+            System.out.println("Digite 3 para buscar um filme:");
+            System.out.println("Digite 4 para exibir a lista de filmes:");
+            System.out.println("Digite 0 para sair:");
+            opcao = in.nextInt();
+
+            switch (opcao) {
+                case 1:
+                    System.out.println("Digite o título do filme:");
+                    filme.setTitulo(in.next());
+                    System.out.println("Digite o gênero do filme:");
+                    filme.setGenero(in.next());
+                    System.out.println("Digite a classificação do filme:");
+                    filme.setClassificacao(in.next());
+                    System.out.println("Digite o ano do filme:");
+                    filme.setAno(in.nextInt());
+
+                    listaFilmes.inserir(filme);
+                    break;
+
+                case 2:
+                    System.out.println("Digite o título do filme a ser removido:");
+                    filme.setTitulo(in.next());
+                    listaFilmes.remover(filme);
+                    break;
+
+                case 3:
+                    System.out.println("Digite o título do filme a ser buscado:");
+                    filme.setTitulo(in.next());
+                    listaFilmes.buscar(filme);
+                    break;
+
+                case 4:
+                    listaFilmes.exibir();
+                    break;
+
+                case 0:
+                    System.out.println("Saindo...");
+                    break;
+
+                default:
+                    System.out.println("Opção inválida.");
+                    break;
+            }
+        } while (opcao != 0);
+    }
+}
+  }
+}
